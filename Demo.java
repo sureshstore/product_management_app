7c1a8a2b5faa6d26bf43b1d6f30e2b546e4630e5
suresh <sureshdares10@gmail.com>
2023-02-26 18:16:47 +0530

    newfile

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String args[]){
+        int i=0;
+        do{
+            System.out.println("Inner Do");
+            System.out.println("next");
+            i++;
+        } while (i<10);
+    }
+}
