7ac77e4e911d4f1b3ef7bb68dbdd06cef2ed3933
suresh <sureshdares10@gmail.com>
2023-02-26 18:22:34 +0530

    dot is added

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ public class Demo{
         int i=0;
         do{
             System.out.println("Inner Do");
-            System.out.println("next");
+            System.out.println("next.");
             i++;
         } while (i<10);
     }
